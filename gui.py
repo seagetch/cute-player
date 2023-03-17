8d905944b41aadbb3b0dba6f92048409b9eaf58f
seagetch <sigetch@gmail.com>
2023-03-18 00:19:58 +0900

    Added some code for physics support and texture thumbnail.

--- gui.py
+++ gui.py
@@ -4,10 +4,13 @@ sys.path.append("inochi2d-py")
 from PySide2 import QtWidgets, QtOpenGL, QtGui, QtCore
 from OpenGL import GL
 import time
-import ctypes
+import numpy as np
 import inochi2d.api as api
 import inochi2d.inochi2d as inochi2d
 import threading
+import json
+import cv2
+#from qt_material import apply_stylesheet
 
 WINDOW_WIDTH = 1300
 WINDOW_HEIGHT = 1200
@@ -23,15 +26,12 @@ class Inochi2DView(QtOpenGL.QGLWidget):
         self.perf_time = None
         self.perf_counter = 0
         self.drag = False
+        self.on_update_tracking = None
 
         self.initialized = False
 
     def initializeGL(self):
-        @ctypes.CFUNCTYPE(ctypes.c_double)
-        def curr_time():
-            return time.perf_counter_ns()
-
-        inochi2d.init(curr_time)
+        inochi2d.init()
 
         if self.onload:
             self.onload(self)
@@ -103,23 +103,11 @@ class Inochi2DView(QtOpenGL.QGLWidget):
                     
         with inochi2d.Scene(0, 0, self.width(), self.height()) as scene:
             self.puppet.update()
+            api.inUpdate()
             self.puppet.draw()
     
-        if self.active_param:
-            kx, ky = self.active_param.get_value()
-            ix, iy = self.active_param.find_closest_keypoint(kx, ky)
-            for bind_item in self.bindings:
-                binding = bind_item.binding
-                name = binding.name()
-                target = binding.node()
-                target_name = target.name()
-                if isinstance(bind_item.value, inochi2d.Deformation):
-                    bind_item.value.pull(ix, iy)
-                    value = bind_item.value
-                else:
-                    bind_item.value = bind_item.binding.get_value(ix, iy)
-                    value = bind_item.value
-                bind_item.setText("%s: %s : %s"%(target_name, name, "%d pts"%len(value.vertex_offsets) if isinstance(value, inochi2d.Deformation) else "%3.2f"%value))
+        if self.on_update_tracking:
+            self.on_update_tracking(self)
         
         time_diff = time.time() - self.perf_time
         if time_diff > 1:
@@ -143,9 +131,14 @@ class ParameterView(QtWidgets.QWidget):
         self.on_select = None
         self.on_update = None
         self.drag      = False
+        self.active    = False
 
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
+        if self.active:
+            rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
+            painter.fillRect(rect, self.palette().highlight())
+
         brush = QtGui.QBrush()
         brush.setColor(QtGui.QColor('#eeeeee'))
         brush.setStyle(QtCore.Qt.SolidPattern)
@@ -170,7 +163,7 @@ class ParameterView(QtWidgets.QWidget):
             painter.setBrush(brush)
             if self.param.is_vec2:
                 x_ratio = (value[0] - self.min[0])/(self.max[0] - self.min[0])
-                y_ratio = (value[1] - self.min[1])/(self.max[1] - self.min[1])
+                y_ratio = 1 - (value[1] - self.min[1])/(self.max[1] - self.min[1])
                 center_x = 8 + dev_w * x_ratio
                 center_y = 8 + 16 + dev_h * y_ratio
             else:
@@ -209,6 +202,7 @@ class ParameterView(QtWidgets.QWidget):
             pos_y = max(rect.top() - 8 - 16, min(pos.y(), rect.bottom() - 8 - 16))
             pos_x = pos_x / rect.size().width()
             pos_y = ( pos_y / rect.size().height() ) if rect.size().height() > 0 else 0
+            pos_y = 1 - pos_y
             x = (self.max[0] - self.min[0]) * pos_x + self.min[0]
             y = (self.max[1] - self.min[1]) * pos_y + self.min[1]
             self.drag = True
@@ -224,6 +218,8 @@ class ParameterView(QtWidgets.QWidget):
 def run(tracker=None):
     app = QtWidgets.QApplication([])
 
+#    apply_stylesheet(app, 'light_blue.xml')
+
     window = QtWidgets.QMainWindow()
     menubar = window.menuBar()
     fileMenu = menubar.addMenu("&File")
@@ -233,38 +229,60 @@ def run(tracker=None):
     window.splitDockWidget(dock_l[0], dock_l[2], QtCore.Qt.Horizontal);
     window.splitDockWidget(dock_l[0], dock_l[1], QtCore.Qt.Vertical);
     window.splitDockWidget(dock_l[2], dock_l[3], QtCore.Qt.Vertical);
-    toolbar = window.addToolBar("Main")
+    toolbar = QtWidgets.QToolBar("Main", window) #window.addToolBar("Main")
+    v_toolbar = QtWidgets.QToolBar("Tool", window)
 
 #    list_widget = QtWidgets.QListWidget(dock_l[0])
     list_container = QtWidgets.QScrollArea(dock_l[0])
     list_widget = QtWidgets.QWidget()
+    list_widget.active_widget = None
     list_container.setWidget(list_widget)
     list_container.setWidgetResizable(True)
     dock_l[0].setWidget(list_container)
     bind_list   = QtWidgets.QListWidget(dock_l[1])
     dock_l[1].setWidget(bind_list)
     tree_widget = QtWidgets.QTreeWidget(dock_l[2])
+    tree_widget.setIconSize(QtCore.QSize(32, 32))
     dock_l[2].setWidget(tree_widget)
     text_area = QtWidgets.QTextEdit(dock_l[3])
     dock_l[3].setWidget(text_area)
 
     def onload(self):
-        self.puppet = inochi2d.Puppet.load("/home/seagetch/ドキュメント/Midori-exporttest-20230304-2.inp")
+        model_name = "/home/seagetch/ドキュメント/Midori-serdetest-20230315.inx"
+#        model_name = "/home/seagetch/ドキュメント/Midori-exporttest-20230304-2.inp"
+        self.puppet = inochi2d.Puppet.load(model_name)
+        self.puppet.set_enable_drivers(True)
+        print("Enable_Drivers=%d"%self.puppet.get_enable_drivers())
         self.active_param = None
-        print("puppet:get_name")
         name = api.inPuppetGetName(self.puppet.handle)
         print(name)
-        print("puppet:root")
         root = self.puppet.root()
-        print("puppet:root:done")
         def dump_json(item, col):
-            text_area.setPlainText(item.node.dumps())
+            text_area.setPlainText(json.dumps(item.node.dumps(False)))
 
         def dump_node(node, parent):
             name = node.name()
             type_id = node.type_id()
             tree_item = QtWidgets.QTreeWidgetItem(["%s: %s"%(type_id, name)])
             tree_item.node = node
+            node_prop = node.dumps(recursive=False)
+            if "textures" in node_prop:
+                texture_id = node_prop["textures"][0]
+                if texture_id <= 65535:
+                    texture = self.puppet.get_texture_from_id(texture_id)
+                    w,h = texture.size()
+                    channels = texture.channels()
+                    buffer, len = texture.get_data()
+                    img = np.ctypeslib.as_array(buffer, (len,))
+                    img = img.reshape([h, w, channels])
+                    scale = min(32/w, 32/h)
+                    icon = cv2.resize(img, None, None, scale, scale)
+                    qimg = QtGui.QImage(icon.data, icon.shape[1], icon.shape[0], icon.strides[0], QtGui.QImage.Format_RGBA8888)
+                    qpixmap = QtGui.QPixmap(qimg)
+                    qicon = QtGui.QIcon(qpixmap)
+                    tree_item.setIcon(0, qicon)
+                    tree_item.setText(0, "%s"%(name))
+
             if parent is None:
                 tree_widget.addTopLevelItem(tree_item)
             else:
@@ -272,7 +290,20 @@ def run(tracker=None):
             for c in node.children():
                 dump_node(c, tree_item)
 
+        dump_node(root, None)
+        tree_widget.itemClicked.connect(dump_json)
+        tree_widget.expandAll()
+
+
         def param_selected(item):
+            if list_widget.active_widget != item:
+                prev_active = list_widget.active_widget
+                if prev_active:
+                    prev_active.active = False
+                    prev_active.update()
+                list_widget.active_widget = item
+                item.active = True
+                list_widget.update()
             bind_list.clear()
             self.active_param = item.param
             bindings = item.param.get_bindings()
@@ -291,11 +322,6 @@ def run(tracker=None):
                 bind_item.setText("%s: %s"%(target_name, name))
                 self.bindings.append(bind_item)
                 bind_item.value = None
-
-        dump_node(root, None)
-        tree_widget.itemClicked.connect(dump_json)
-        tree_widget.expandAll()
-
 
         print("puppet:parameters")
         params = self.puppet.parameters()
@@ -322,11 +348,50 @@ def run(tracker=None):
 
         self.timer = 0
 
+    main_container = QtWidgets.QWidget(window)
+    sub_container = QtWidgets.QWidget(window)
+
+    main_vbox = QtWidgets.QVBoxLayout()
+    main_container.setLayout(main_vbox)
+    main_vbox.addWidget(toolbar)
+    main_vbox.addWidget(sub_container)
+
+    main_hbox = QtWidgets.QHBoxLayout()
+    sub_container.setLayout(main_hbox)
+    main_hbox.addWidget(v_toolbar)
+    v_toolbar.setOrientation(QtCore.Qt.Vertical)
+
     gl_widget = Inochi2DView(window)
     gl_widget.statusbar = statusbar
-    window.setCentralWidget(gl_widget)
+    main_hbox.addWidget(gl_widget)
+
+    v_toolbar.addAction(QtWidgets.QAction("Parts Layout", window))
+    v_toolbar.addAction(QtWidgets.QAction("Transformation", window))
+    v_toolbar.addAction(QtWidgets.QAction("Animation Timeline", window))
+
+#    window.setCentralWidget(gl_widget)
+    window.setCentralWidget(main_container)
     gl_widget.onload = onload
     gl_widget.tracker = tracker
+
+    def on_update_tracking(self):
+        if self.active_param:
+            kx, ky = self.active_param.get_value()
+            ix, iy = self.active_param.find_closest_keypoint(kx, ky)
+            for bind_item in self.bindings:
+                binding = bind_item.binding
+                name = binding.name()
+                target = binding.node()
+                target_name = target.name()
+                if isinstance(bind_item.value, inochi2d.Deformation):
+                    bind_item.value.pull(ix, iy)
+                    value = bind_item.value
+                else:
+                    bind_item.value = bind_item.binding.get_value(ix, iy)
+                    value = bind_item.value
+                bind_item.setText("%s: %s : %s"%(target_name, name, "%d pts"%len(value.vertex_offsets) if isinstance(value, inochi2d.Deformation) else "%3.2f"%value))
+
+#    gl_widget.on_update_tracking = on_update_tracking
 
     def toggle_tracking(self):
         if gl_widget.tracker.terminate:
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import gui
 import threading
 if __name__ == "__main__":
     tracker = facetracker.FaceTracker()
-    th = threading.Thread(target=tracker.run)
-    th.start()
+    tracker.terminate = True
+#    th = threading.Thread(target=tracker.run)
+#    th.start()
     gui.run(tracker)
